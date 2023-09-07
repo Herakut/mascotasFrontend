674c9c963d07cc2a9acef24887d604e80db083f0
dcritoballl <d.cristobalgallego@gmail.com>
2023-09-07 14:55:47 +0200

    done

--- src/context/auth.context.jsx
+++ src/context/auth.context.jsx
@@ -23,7 +23,8 @@ function AuthWrapper(props) {
     try {
       
       const response = await service.get("/auth/verify")
-      console.log(response)
+      
+      console.log(response, "AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
 
       setIsUserActive(true)
       setActiveUserId(response.data._id)
